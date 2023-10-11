7271a6a2aac678c295854d82b1ae2f6f47097e1e
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-10-11 14:23:39 -0300

    feat: task table

---
+++ src/main/java/br/com/ederbiason/todolist/task/TaskModel.java
@@ -0,0 +1,30 @@
+package br.com.ederbiason.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name="tb_tasks")
+public class TaskModel {
+    @Id
+    @GeneratedValue(generator="UUID")
+    private UUID id;
+
+    @Column(length=50)
+    private String title;
+    private String description, priority;
+    private LocalDateTime startAt, endAt;
+    
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
